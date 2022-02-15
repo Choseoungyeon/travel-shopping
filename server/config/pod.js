8c0b62519346c50c9e1aee1c598ebdd41f4b16b6
Cho <bottle34@naver.com>
2022-02-16 04:38:40 +0900

    heroku deployment

--- server/config/pod.js
+++ server/config/pod.js
@@ -1,4 +1,6 @@
-export const mongoURI = process.env.MONGO_URI;
-export const cloudName = process.env.CLOUD_NAME;
-export const apiKey = process.env.API_KEY;
-export const apiSecret = process.env.API_SECRETE;
+module.exports = {
+    mongoURI:process.env.MONGO_URI,
+    cloudName: process.env.CLOUD_NAME,
+    apiKey: process.env.API_KEY,
+    apiSecret: process.env.API_SECRETE
+}
