6a95b787f59d65bd47208825faa50b6dde906679
Cho <bottle34@naver.com>
2022-02-16 13:39:50 +0900

    heroku deployments

--- server/index.js
+++ server/index.js
@@ -334,12 +334,15 @@ app.get('/api/users/removeCart', auth, (req, res) => {
 })
 
 if (process.env.NODE_ENV === "production") {
-  const root = require('path').join(__dirname, "../client","build");
-  app.use(express.static(root))
-  app.get("*",(req, res)=>{
-    res.sendFile("index.html",{root});
-  })
-  }
+
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  // index.html for all page routes
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  });
+}
 
 
 app.listen(port, () => {
